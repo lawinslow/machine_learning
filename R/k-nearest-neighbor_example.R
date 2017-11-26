bb42502ab7578307023378f0b2953fb63ffaa6e6
Luke Winslow <lawinslow@gmail.com>
2017-11-26 15:21:01 -0500

    add example for knn classification

---
+++ R/k-nearest-neighbor_example.R
@@ -0,0 +1,33 @@
+library(dplyr)
+library(class)
+library(caret)
+
+###############################################################################
+# Load and prep data
+###############################################################################
+data = read.csv('data/g19_2007_bigtable.csv', as.is=TRUE)
+
+trophic_info = data %>% select(TROPHIC_STATE, NTL, PTL, CHLA, TURB, DO_SURF, DOC) %>%
+  filter(TROPHIC_STATE != '') %>% na.omit
+trophic_info$TROPHIC_STATE = factor(trophic_info$TROPHIC_STATE)
+
+
+###############################################################################
+# Split data into training and testing datasets
+###############################################################################
+train_i = sample(1:nrow(trophic_info), size = floor(nrow(trophic_info)/2))
+
+train = trophic_info[train_i, ]
+test = trophic_info[-train_i, ]
+
+
+###############################################################################
+# Run against k-nearest neighbors and examing results
+###############################################################################
+
+## number of K can be changed for different results
+est = knn(train[,-1], test[,-1], train$TROPHIC_STATE, k = 3, prob=FALSE)  
+
+confusionMatrix(data=est, test$TROPHIC_STATE)
+
+View(data.frame(data=est, reference=test$TROPHIC_STATE))
